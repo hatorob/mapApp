bb1a89f573cd854b7e7966e3c7ee28b9126332a9
Hector Toro <alejotoro94@hotmail.com>
2024-04-04 19:02:14 -0500

    Se agrega la funcionalidad del zoom

--- src/app/maps/pages/zoom-page/zoom-page.component.ts
+++ src/app/maps/pages/zoom-page/zoom-page.component.ts
@@ -10,19 +10,58 @@ export class ZoomPageComponent implements AfterViewInit{
   @ViewChild('map')
   public divMap?: ElementRef;
 
-
+  //! valor inicial del zoom
+  public zoom: number = 10;
+  public map?: Map;
 
   ngAfterViewInit(): void {
 
     //! si no existe hagame un return
     if( !this.divMap ) return;
 
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListeners();
+
   }
+
+
+  //!listener del map para escuchar los cambios
+
+  public mapListeners = ():void => {
+
+    if( !this.map ) throw "Mapa no inicializado";
+
+    this.map.on('zoom', (env) => {
+      //console.log({env});
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (env) => {
+      if( this.map!.getZoom() < 18 ) return;
+      this.map!.zoomTo(18);
+    });
+
+  }
+
+  public zoomIn = ():void => {
+    this.map?.zoomIn();
+  }
+
+  public zoomOut = ():void => {
+    this.map?.zoomOut();
+  }
+
+  /* Para el control de la barra del input */
+  public zoomChanged = ( zoomValue: string ): void => {
+    this.zoom = Number(zoomValue);
+    this.map?.zoomTo(this.zoom)
+  }
+
 
 }
