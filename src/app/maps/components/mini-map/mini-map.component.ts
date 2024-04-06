130d56f64ee65e4e3e971e3fe27bda3518a6aaaa
Hector Toro <alejotoro94@hotmail.com>
2024-04-06 18:03:59 -0500

    Se termina los mapas para renderizar minimapas

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
+
+interface MarketAndColor {
+  market: Marker;
+  color: string;
+}
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  //!Valores iniciales
+  public zoom: number = 15;
+  public map?: Map;
+
+
+  ngAfterViewInit(): void {
+
+    if( !this.divMap ) throw "No se cargo el mapa"
+    if( !this.lngLat ) throw "No envió las coordenadas"
+
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
+
+    })
+
+    const marker = new Marker({
+      color: '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16)),
+    }).setLngLat(this.lngLat)
+      .addTo( map );
+  }
+
+
 
 }
